07de0b6bd4d9ddbb8aff05a060ebbcff8d7b7762
Minju <69795199+ks00919@users.noreply.github.com>
2024-10-17 10:31:55 +0900

    [Bronze II] Title: 피보나치 수, Time: 80 ms, Memory: 11484 KB -BaekjoonHub

---
+++ 백준/Bronze/2747. 피보나치 수/피보나치 수.java
@@ -0,0 +1,21 @@
+import static java.lang.Integer.*;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = parseInt(br.readLine());
+
+		int[] memo = new int[n + 1];
+		memo[1] = 1;
+		for (int i = 2; i <= n; i++) {
+			memo[i] = memo[i - 2] + memo[i - 1];
+		}
+
+		System.out.println(memo[n]);
+
+	}
+}
