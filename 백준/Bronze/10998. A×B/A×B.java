9b47800605af0d21bb892d132e732f8e98729011
Minju <69795199+ks00919@users.noreply.github.com>
2024-10-17 10:35:00 +0900

    [Bronze V] Title: A×B, Time: 84 ms, Memory: 11532 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.java
@@ -0,0 +1,14 @@
+import static java.lang.Integer.*;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        System.out.println(parseInt(st.nextToken()) * parseInt(st.nextToken()));
+    }
+}
