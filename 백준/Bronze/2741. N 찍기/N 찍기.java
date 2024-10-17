f1a90b05a8bbd2f4fe28002ba81b34642173e5c1
Minju <69795199+ks00919@users.noreply.github.com>
2024-10-17 10:38:58 +0900

    [Bronze V] Title: N 찍기, Time: 168 ms, Memory: 17168 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -0,0 +1,17 @@
+import static java.lang.Integer.parseInt;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = parseInt(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		for (int i = 1; i <= N; i++) {
+			sb.append(i).append("\n");
+		}
+		System.out.println(sb);
+	}
+}
