f0d70959ccab12da22d487957bc7c1c6865bbf2e
Minju <69795199+ks00919@users.noreply.github.com>
2025-01-18 00:21:33 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 60 ms, Memory: 11464 KB -BaekjoonHub

---
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static int[][] dp = new int[15][15];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < 15; i++) {
+            dp[0][i] = i;
+        }
+
+        for (int i = 1; i < 15; i++) {
+            for (int j = 1; j < 15; j++) {
+                dp[i][j] = dp[i][j - 1] + dp[i - 1][j];
+            }
+        }
+
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < t; i++) {
+            int k = Integer.parseInt(br.readLine());
+            int n = Integer.parseInt(br.readLine());
+
+            sb.append(dp[k][n]).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
